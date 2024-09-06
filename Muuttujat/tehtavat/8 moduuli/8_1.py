b0427fb223e150f442c2475ab7e1935289a68dee
eKuuuuu <erik.roigas@metropolia.fi>
2024-09-06 13:01:47 +0300

    tehtavat

--- Muuttujat/tehtavat/8 moduuli/8_1.py
+++ Muuttujat/tehtavat/8 moduuli/8_1.py
@@ -1,5 +1,13 @@
 import mysql.connector
 
+def ICAO_koodi(syote):
+    sql =  f"SELECT name FROM ident WHERE ident='{syote}'"
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    for rivi in tulos:
+        print(f"ICAO koodi: {syote} vastaa lentoasemaa {rivi[0]}")
+
 yhteys = mysql.connector.connect(
          host='127.0.0.1',
          port= 3306,
@@ -8,3 +16,6 @@ yhteys = mysql.connector.connect(
          password='password',
          autocommit=True
          )
+
+syote = input("Anna etsivänä lentoaseman ICAO-koodi:")
+ICAO_koodi(syote)

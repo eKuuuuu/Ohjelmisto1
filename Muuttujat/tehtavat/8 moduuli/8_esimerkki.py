e13f8ce273503f5272a2ce00ca535ea0574c8891
eKuuuuu <erik.roigas@metropolia.fi>
2024-09-06 11:00:30 +0300

    tehtavat

--- Muuttujat/tehtavat/8 moduuli/8_esimerkki.py
+++ Muuttujat/tehtavat/8 moduuli/8_esimerkki.py
@@ -1,5 +1,20 @@
 import mysql.connector
 
+def maakoodi(nimi):
+    sql = f"SELECT iso_country, continent FROM country where name='{nimi}'"
+    print(sql)
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    for rivi in tulos:
+        print(f"Maan {nimi} iso_country koodi on {rivi[0]} ja maa sijaitsee {rivi[1]}:ssa")## __ = {rivi[1]}
+
+def tietokannat(sql):
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    return tulos
+
 yhteys = mysql.connector.connect(
          host='127.0.0.1',
          port= 3306,
@@ -9,15 +24,9 @@ yhteys = mysql.connector.connect(
          autocommit=True
          )
 
-def maakoodi(nimi):
-    sql = f"SELECT iso_country FROM country where name='{nimi}'"
-    print(sql)
-    kursori = yhteys.cursor()
-    kursori.execute(sql)
-    tulos = kursori.fetchall()
-    if kursori.rowcount > 0 :
-        for rivi in tulos:
-            print(f"Maan {nimi} iso_country koodi on {rivi[0]} ja maa sijaitsee __:ssa")## __ = {rivi[1]}
-
 maa = input("Anna maan nimi mistä olet kiinnostunut: ")
 maakoodi(maa)
+sql = f"SELECT iso_country, continent FROM country where name='{maa}'"
+tulos = tietokannat(sql)
+for rivi in tulos:
+    print(f"Maan {maa} iso_country koodi on {rivi[0]} ja maa sijaitsee {rivi[1]}:ssa")  ## __ = {rivi[1]}

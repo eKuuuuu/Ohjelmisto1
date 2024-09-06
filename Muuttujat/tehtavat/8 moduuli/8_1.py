9edc53d1217084325beada2d619b4d265a643be6
eKuuuuu <erik.roigas@metropolia.fi>
2024-09-06 10:42:16 +0300

    tehtavat

--- Muuttujat/tehtavat/8 moduuli/8_1.py
+++ Muuttujat/tehtavat/8 moduuli/8_1.py
@@ -1,4 +1,4 @@
-import mysql.connector()
+import mysql.connector
 
 yhteys = mysql.connector.connect(
          host='127.0.0.1',
@@ -8,3 +8,16 @@ yhteys = mysql.connector.connect(
          password='password',
          autocommit=True
          )
+
+def maakoodi(nimi):
+    sql = f"SELECT iso_country FROM country where name='{nimi}'"
+    print(sql)
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    if kursori.rowcount > 0 :
+        for rivi in tulos:
+            print(f"Maan {nimi} iso_country koodi on {rivi[0]} ja maa sijaitsee {rivi[1]}:ssa")
+
+maa = input("Anna maan nimi mistä olet kiinnostunut: ")
+maakoodi(maa)
